6e0e869a1ea75c7593c845b59fbe24cc3dd3b741
MSe1969 <mse1969@posteo.de>
2018-12-08 13:21:17 +0100

    Introduce Bromite SystemWebView
    Based on https://github.com/MSe1969/android_external_bromite-webview . The upstream repo is already around ~1.5 GB and I don't want to drag this around.
    
    Okay, but where are the apks ? A script fetches the latest apks from bromite releases into the repo.

--- product.mk
+++ product.mk
@@ -1,5 +1,9 @@
 VENDOR_EXTRA_PATH := vendor/extra
 
+# Custom packages
+PRODUCT_PACKAGES += \
+    bromite-webview
+
 # Custom bootanimation
 PRODUCT_COPY_FILES += \
     $(VENDOR_EXTRA_PATH)/bootanimation/bootanimation.zip:system/product/media/bootanimation.zip
