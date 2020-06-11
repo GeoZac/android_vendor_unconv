f0d4ab54dba62177fae9d407308bd9e34ca644e0
George Zacharia <george.zcharia@gmail.com>
2020-06-11 21:29:23 +0530

    unconv: update adblock
    update adblock hosts with my changes, optimizing it in the process

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,8 @@ echo "Extra vendor detected"
 
 # Fetch latest bormite webview apks
 /usr/bin/python3 vendor/extra/scripts/latestbromite.py
+
+# Update custom hosts including my changes
+if [ -d "external/hosts/" ]; then
+    /usr/bin/python3 external/hosts/updateHostsFile.py -n -m
+fi
