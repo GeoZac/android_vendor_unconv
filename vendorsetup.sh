e284eada361b0487f6ae3264b02cd1290e45660e
George Zacharia <george.zcharia@gmail.com>
2020-08-23 22:29:33 +0530

    unconv: Well some secrets were spilled
    Explicitly mention that we skip changelog generation

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,5 +8,9 @@ if [ -d "external/hosts/" ]; then
     /usr/bin/python3 external/hosts/updateHostsFile.py -n -m
 fi
 
+# Don't bother,get the zip ready without haste
+export USER_BUILD_NO_CHANGELOG=1
+echo "Changelog will be skipped"
+
 # Build my custom Ad-block file
 export INPUT_ADBLOCK_FILE=external/hosts/hosts_unconv
