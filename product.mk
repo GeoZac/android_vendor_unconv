f5df015bdfced2512a6bbb171a7d70c4b2e6b690
George Zacharia <george.zcharia@gmail.com>
2020-12-07 19:08:18 +0530

    unconv: Add some tunes
    No need to keep a file around in sdcard
    
    The final name is changed as AOSP already ships a file with same name

--- product.mk
+++ product.mk
@@ -27,3 +27,7 @@ ADDITIONAL_FONTS_FILE += \
 # Exclude some undesired packages
 PRODUCT_PACKAGES += \
     NukePackages
+
+# Add some nostalgic tunes
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/sounds/ringtones/ogg/Dione_48k.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Unconv.ogg
