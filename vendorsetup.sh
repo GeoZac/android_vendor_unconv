e3fe6335bb4e077af2943ba62da0d7cc3b00f557
George Zacharia <george.zcharia@gmail.com>
2021-01-02 11:54:48 +0530

    vendorsetup: Set custom ad-block only if modified
    There is not point in setting the custom host if I'm not modifying anything

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,11 +9,11 @@ if [ -d "external/hosts/extensions/unconv/" ]; then
     /usr/bin/python3 external/hosts/updateHostsFile.py -n -m
     echo "Replacing duplicates domains with wildcards in hosts file"
     /usr/bin/python3 vendor/extra/scripts/wildcardhosts.py external/hosts/hosts_unconv vendor/extra/adaway/
+	
+	# Build my custom Ad-block file
+	export INPUT_ADBLOCK_FILE=vendor/extra/adaway/hosts_unconv_w
 fi
 
 # Don't bother,get the zip ready without haste
 export USER_BUILD_NO_CHANGELOG=1
 echo "Changelog will be skipped"
-
-# Build my custom Ad-block file
-export INPUT_ADBLOCK_FILE=vendor/extra/adaway/hosts_unconv_w
