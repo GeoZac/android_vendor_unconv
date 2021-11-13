746d67ce763311e5044485bd8f64c441c34fa097
George Zacharia <george.zcharia@gmail.com>
2021-11-13 07:58:17 +0530

    vendorsetup: Make my scripts available in all of environment
    Two essential ones for now

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,3 +17,13 @@ fi
 # Don't bother,get the zip ready without haste
 export USER_BUILD_NO_CHANGELOG=1
 echo "Changelog will be skipped"
+
+function cherrypicker() {
+    T=$(gettop)
+    $T/vendor/extra/scripts/cherrypicker.py $@
+}
+
+function logspammers() {
+    T=$(gettop)
+    $T/vendor/extra/scripts/logspammers.py $@
+}
