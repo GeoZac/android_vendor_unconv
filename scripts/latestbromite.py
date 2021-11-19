eb94b0d3ecefc2636197d9f56a1f41f338d8f91e
George Zacharia <george.zcharia@gmail.com>
2021-11-19 12:32:01 +0530

    scripts: latestbromite: Cleanup
    Fix typos
    Refactor function names
    refactor variable names

--- scripts/latestbromite.py
+++ scripts/latestbromite.py
@@ -13,13 +13,13 @@ VERIFY = False
 BASE_PATH = "vendor/extra/prebuilt/apps/bromite-webview/"
 
 
-def writetag(tag):
-    fname = BASE_PATH + "bromite_version.txt"
-    with open(fname, "w") as file_write:
+def write_tag(tag):
+    f_name = BASE_PATH + "bromite_version.txt"
+    with open(f_name, "w") as file_write:
         file_write.write(tag)
 
 
-def getfilehash(file_name):
+def get_file_hash(file_name):
     with open(file_name, "rb") as file:
         sha256_hash = sha256()
         # Read and update hash string value in blocks of 4K
@@ -28,11 +28,11 @@ def getfilehash(file_name):
         print("Filename:", file_name, "\nSHA256 checksum :", sha256_hash.hexdigest(), "\n")
 
 
-def fetchfile(url, filename, filesize):
+def fetch_file(url, filename, file_size):
     asset_file_data = get(url, stream=True)
     with open(filename, "wb") as out_file:
         print(filename)
-        total_length = int(filesize)
+        total_length = int(file_size)
         for chunk in bar(
                 asset_file_data.iter_content(chunk_size=4096),
                 expected_size=(total_length / 4096) + 1,
@@ -43,7 +43,7 @@ def fetchfile(url, filename, filesize):
     out_file.close()
 
 
-def getassethashes(assets, files_to_hash):
+def get_asset_hashes(assets, files_to_hash):
     for asset in assets:
         hashes = {}
         if "sha256.txt" in asset["name"]:
@@ -56,87 +56,87 @@ def getassethashes(assets, files_to_hash):
     return None
 
 
-def updateassets(tag, assets):
+def update_assets(tag, assets):
     asset_names = ["arm64_SystemWebView.apk", "arm_SystemWebView.apk"]
-    asset_hashes = getassethashes(assets, asset_names)
+    asset_hashes = get_asset_hashes(assets, asset_names)
     for asset in assets:
         asset_name = asset["name"]
         filename = BASE_PATH + asset_name.split("_")[0] + "/SystemWebView.apk"
-        filesize = asset["size"]
+        file_size = asset["size"]
         if not any(item in asset_name for item in asset_names):
             if DEBUG:  # Silently continue in case not DEBUG
                 print(f"Skipped {asset_name}")
             continue
         # Assets will have same name, just check for SHA256 hash too
-        if isfile(filename) and getfilehash(filename) == asset_hashes[asset_name]:
+        if isfile(filename) and get_file_hash(filename) == asset_hashes[asset_name]:
             print(f"{filename} already up-to date")
         else:
             if DEBUG:
-                print(asset["browser_download_url"], filename, filesize)
+                print(asset["browser_download_url"], filename, file_size)
 
             else:
-                fetchfile(asset["browser_download_url"], filename, filesize)
+                fetch_file(asset["browser_download_url"], filename, file_size)
                 print(f"Updated {asset_name} to v{tag}")
 
         if VERIFY:
-            getfilehash(filename)
+            get_file_hash(filename)
 
     if not DEBUG:
-        writetag(tag)
+        write_tag(tag)
 
 
-def getlatestbromite():
+def get_latest_bromite():
     repo_name = "bromite/bromite"
     repo_url = f"https://api.github.com/repos/{repo_name}/releases"
     data = get(repo_url).json()
     index = 0
     tag_name = data[index]["tag_name"]
-    prerelease = data[index]["prerelease"]
-    fname = BASE_PATH + "bromite_version.txt"
-    if exists(fname):
-        if prerelease:
+    pre_release = data[index]["prerelease"]
+    f_name = BASE_PATH + "bromite_version.txt"
+    if exists(f_name):
+        if pre_release:
             # Skip pre-release versions
             return
-        with open(fname, "r") as file_read:
+        with open(f_name, "r") as file_read:
             current_version = file_read.readline()
             print(f"Current Version: {current_version}")
     else:
         # Since we have no priors version,apply a sane value to check
         current_version = "0.0.0.0"
         # Also check if the latest version is a pre-release, in which case, get the previous version
-        if prerelease:
+        if pre_release:
             index += 1
             tag_name = data[index]["tag_name"]
 
     print(f"Latest version : {tag_name}")
-    updateallowed = False
+    update_allowed = False
     if version.parse(tag_name) > version.parse(current_version):
         try:
             console_input = inputimeout(prompt=f"Update apk assets to {tag_name}? :", timeout=5)
         except TimeoutOccurred:
             console_input = "n"
-        updateallowed = console_input == "y"
-    if updateallowed:
-        updateassets(tag_name, data[index]["assets"])
+        update_allowed = console_input == "y"
+    if update_allowed:
+        update_assets(tag_name, data[index]["assets"])
 
 
-def checkfileexits():
+def check_file_exists():
     # On a fresh clone the files are not supposed to exist, so get the latest tag, get the assets,mark the version
     if not exists(BASE_PATH + "bromite_version.txt"):
-        print("Seems not initilized, will do it now")
-        getlatestbromite()
+        print("Seems not initialised, will do it now")
+        get_latest_bromite()
         return False
     print("File Exists, will see about updating")
     return True
 
 
 def latest_bromite():
-    initilized = checkfileexits()
+    initialised = check_file_exists()
 
-    if initilized:
-        getlatestbromite()
+    if initialised:
+        get_latest_bromite()
     else:
-        print("Everthing in order")
+        print("Everything in order")
 
 
 if __name__ == "__main__":
